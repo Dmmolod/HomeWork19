39a63395338d0003f766089d66266c8a0af39cc3
Dmitry <d.v.molodetskiy@gmail.com>
2022-02-02 13:28:23 +0300

    Вторая версия: изменил модель, перенес основные функции в контроллер, изменил класс ImageScreen - убрал автоматическое сохрание данных, перенес это в модель. Добавил анимации листания, установил делегат только для View. Убрал класс UserActionsController - перенес реализацию функций делегата UserActionsDelegate сразу в контроллер

---
+++ HomeWork19/SwipeSide.swift
@@ -0,0 +1,14 @@
+//
+//  SwipeSide.swift
+//  HomeWork19
+//
+//  Created by Дмитрий Молодецкий on 01.02.2022.
+//
+
+import Foundation
+import UIKit
+
+enum SwipeSide {
+    case left
+    case right
+}
